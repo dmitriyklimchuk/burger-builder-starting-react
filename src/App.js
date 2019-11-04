e923b208df70f5202261249e2e1522681725ab16
Kim <dmitriyklimchukzt@gmail.com>
2019-11-04 18:40:22 +0200

    Starting Implementation of Burger Builder Container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from "./containers/BurdgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <h1>Test</h1>
+          <BurgerBuilder/>
         </Layout>
       </div>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary'
 
+import classes from './Layout.css'
+
 const layout = (props) => (
   <Aux>
     <div>Toolbar, Sidebar, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
