ba5458cb70b9e53d1a2d38c2be3833de786d83b8
Kim <dmitriyklimchukzt@gmail.com>
2019-11-10 08:07:30 +0200

    Implementing Backdrop (Overlay) Component

--- src/containers/BurdgerBuilder/BurgerBuilder.js
+++ src/containers/BurdgerBuilder/BurgerBuilder.js
@@ -90,6 +90,12 @@ class BurgerBuilder extends Component {
     });
   };
 
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  };
+
 
   render () {
 
@@ -105,7 +111,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients = {this.state.ingredients}/>
